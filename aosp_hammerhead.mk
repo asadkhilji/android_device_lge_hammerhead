c73b3437146f618d11221be51e3f8aefbc44760b
zeDDer <2339201+zeDDer@users.noreply.github.com>
2019-09-05 03:21:38 +0300

    hammerhead: PE-ify

--- aosp_hammerhead.mk
+++ aosp_hammerhead.mk
@@ -1,22 +1,35 @@
-#
-# Copyright 2013 The Android Open-Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOTANIMATION_HALF_RES := true
+
+<<<<<<< HEAD:lineage_hammerhead.mk
+# Exclude AudioFX app
+TARGET_EXCLUDES_AUDIOFX := true
+=======
+TARGET_USES_AOSP := true
+TARGET_GAPPS_ARCH := arm
+TARGET_MINIMAL_APPS := true
+
+# Inherit basic aosp junk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+>>>>>>> 1cbbebf... hammerhead: PE-ify:aosp_hammerhead.mk
+
+# Inherit some common aosp stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := hammerhead
 PRODUCT_NAME := aosp_hammerhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5
+PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_PACKAGES += \
-    Launcher3
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=hammerhead \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 M4B30Z 3437181 release-keys"
 
+BUILD_FINGERPRINT := google/hammerhead/hammerhead:6.0.1/M4B30Z/3437181:user/release-keys
